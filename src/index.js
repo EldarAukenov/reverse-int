691b0d558363ca04f9f26df8296f18ae1408b538
EldarAukenov <119871117+EldarAukenov@users.noreply.github.com>
2023-02-24 17:23:57 +0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const isNegative = n < 0;
+    const absN = Math.abs(n);
+    const reversed = Number(absN.toString().split("").reverse().join(""));
+    return isNegative ? reversed : reversed * Math.sign(n);
+};
